3db95c2ba3911ff7ae0e4040e9f7d4493dc09a77
Aaron Perez <aap7640@gmail.com>
2022-11-08 01:18:26 -0600

    Moved some functions from lib.c to shaders.c

--- include/lib.h
+++ include/lib.h
@@ -12,9 +12,5 @@ void processInput(GLFWwindow *);
 void framebuffer_size_callback(GLFWwindow *, int, int);
 void error_callback(char *, char *);
 void info_callback(char *);
-void build_vertex_shader(FILE *, char **, unsigned int, unsigned int *);
-void build_fragment_shader(FILE *, char **, unsigned int, unsigned int *);
-int compile_shaders(unsigned int *, unsigned int *, unsigned int *);
-
 
 #endif
--- include/shaders.h
+++ include/shaders.h
@@ -11,5 +11,8 @@
 #define NULL_CHAR '\0'
 
 int parse_shader(FILE *, char **, int);
+void build_vertex_shader(FILE *, char **, unsigned int, unsigned int *);
+void build_fragment_shader(FILE *, char **, unsigned int, unsigned int *);
+int compile_shaders(unsigned int *, unsigned int *, unsigned int *);
 
 #endif
